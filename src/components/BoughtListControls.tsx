61cb1894394c470b9353852f1ad7cefb3a6b87eb
En-Chi Liu <enchi.liu@gmail.com>
2021-08-24 11:41:17 +0200

    toggle showBought straight from onClick

--- src/components/BoughtListControls.tsx
+++ src/components/BoughtListControls.tsx
@@ -17,18 +17,11 @@ export default function BoughtListControls() {
 
   const iconToDisplay = showBought ? closedEye : openEye;
 
-  // Functions
-  // you use functions in App.tsx but const here.
-  // for consistency only use one method. In this case, use functions because it makes easy to indentify them in VS code outline mode (i will explain this in the lectures)
-  // also this should go after the contants declared below (hoisting) properties always go on top.
-  // -1
-  const toggleBoughtList = () => setShowBought(!showBought);
-
   return (
     <section className="BoughtListControls">
       <button
         type="button"
-        onClick={toggleBoughtList}
+        onClick={() => setShowBought(!showBought)}
         className="button-subtle"
       >
         <img src={iconToDisplay} alt="" className="icon" />
